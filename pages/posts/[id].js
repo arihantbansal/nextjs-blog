768bf6b91f8d91bfc67fb570a0a71008554363ba
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-27 21:54:21 +0530

    Fix typos

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
-import Layout from "../../components/layout";
+import Layout from "../../components/Layout";
 import Date from "../../components/Date";
 import utilStyles from "../../styles/utils.module.css";
 
